4e2330bff1a5dc98c134889818769ca52897cef4
cklare <67334145+cklare@users.noreply.github.com>
2022-07-05 12:33:45 -0400

    Add files via upload
    Updated 7/5

--- firstcode.py
+++ firstcode.py
@@ -1,5 +1,4 @@
 # take 2 - the last file got corrupted
-import sys
 import numpy as np
 import matplotlib.pyplot as plt
 import os
@@ -8,9 +7,9 @@ from icecube import dataio, dataclasses
 from I3Tray import *
 from glob import glob
 
-data_folder = '/scratch/cklare/tau_data/'
 tray = I3Tray()
-tray.Add("I3Reader", filename="evel2_IC86.2020_NuTau.021989.000599.i3")
+tray.Add("I3Reader", filename="evel2_IC86.2020_NuTau.021989.000031.i3")
 tray.Add("Dump","dump")
 tray.Execute()
+
 
